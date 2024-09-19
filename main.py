d4d58cd97b451c74e0a1889bdb0480fac3d8b037
mrubin01 <mi.rubino@protonmail.com>
2024-09-19 20:35:00 +0100

    Added sector and industry, increased the yearly yield filter to 10%

--- main.py
+++ main.py
@@ -20,124 +20,143 @@ stock_tickers = read_stock_tickers('stocks.txt')
 # stock_data = [fetch_stock_data(ticker) for ticker in stock_data]
 
 # uncomment the code below to fetch info for a stock
-# stock = yf.Ticker("AAPL")
-# print(stock.info)
-# sys.exit()
+stock = yf.Ticker("ASC")
+print(stock.info)
+sys.exit()
+
 
 stock_data = []
 stock_with_no_yield = []
+""" Instead of using the function fetch_stock_data, the metrics have been downloaded one
+by one to deal with missing data. The function throws an error even for one missing metric,
+while this way the tickers with missing data will be used as well and the missing metric 
+will be set to -999999 """
 
 for t in stock_tickers:
-    stock = yf.Ticker(t)
+    if t not in ("AIF", "AFT"):
+        stock = yf.Ticker(t)
 
-    #
-    try:
-        if stock.info["shortName"] and isinstance(stock.info["shortName"], str):
-            name = stock.info["shortName"]
-        elif stock.info["longName"] and isinstance(stock.info["longName"], str):
-            name = stock.info["longName"]
-    except KeyError:
-        name = "missing"
+        try:
+            if stock.info["shortName"] and isinstance(stock.info["shortName"], str):
+                name = stock.info["shortName"]
+            elif stock.info["longName"] and isinstance(stock.info["longName"], str):
+                name = stock.info["longName"]
+        except KeyError:
+            name = "missing"
 
-    try:
-        if stock.info["beta"] and not isinstance(stock.info["beta"], str):
-            beta = round(stock.info["beta"], 2)
-    except KeyError:
-        beta = -999999
+        try:
+            if stock.info["industry"] and isinstance(stock.info["industry"], str):
+                industry = stock.info["industry"]
+        except KeyError:
+            industry = "missing"
 
-    try:
-        if stock.info["currentPrice"] and not isinstance(stock.info["currentPrice"], str):
-            price = round(stock.info["currentPrice"], 2)
-    except KeyError:
-        price = -999999
+        try:
+            if stock.info["sector"] and isinstance(stock.info["sector"], str):
+                sector = stock.info["sector"]
+        except KeyError:
+            sector = "missing"
 
-    # Fetch the metrics, otherwise set them to -999999
-    try:
-        if stock.info["dividendYield"] and not isinstance(stock.info["dividendYield"], str):
-            div_yield = round(stock.info["dividendYield"] * 100, 2)
-    except KeyError:
-        div_yield = -999999
+        try:
+            if stock.info["beta"] and not isinstance(stock.info["beta"], str):
+                beta = round(stock.info["beta"], 2)
+        except KeyError:
+            beta = -999999
 
-    if div_yield != -999999:
-        yearly_div_yield = round((div_yield / current_month) * 12, 2)
-    else:
-        yearly_div_yield = -999999
+        try:
+            if stock.info["currentPrice"] and not isinstance(stock.info["currentPrice"], str):
+                price = round(stock.info["currentPrice"], 2)
+        except KeyError:
+            price = -999999
 
-    try:
-        if stock.info["payoutRatio"] and not isinstance(stock.info["payoutRatio"], str):
-            pay_ratio = round(stock.info["payoutRatio"] * 100, 2)
-    except KeyError:
-        pay_ratio = -999999
+        # Fetch the metrics, otherwise set them to -999999
+        try:
+            if stock.info["dividendYield"] and not isinstance(stock.info["dividendYield"], str):
+                div_yield = round(stock.info["dividendYield"] * 100, 2)
+        except KeyError:
+            div_yield = -999999
 
-    if pay_ratio != -999999:
-        div_coverage = round((1 / pay_ratio) * 100, 2)
-    else:
-        div_coverage = -999999
+        if div_yield != -999999:
+            yearly_div_yield = round((div_yield / current_month) * 12, 2)
+        else:
+            yearly_div_yield = -999999
 
-    div_growth_rate = round(calculate_dividend_growth_rate(stock), 2)
+        try:
+            if stock.info["payoutRatio"] and not isinstance(stock.info["payoutRatio"], str):
+                pay_ratio = round(stock.info["payoutRatio"] * 100, 2)
+        except KeyError:
+            pay_ratio = -999999
 
-    try:
-        if stock.info["trailingEps"] and not isinstance(stock.info["trailingEps"], str):
-            eps = round(stock.info["trailingEps"], 2)
-    except KeyError:
-        eps = -999999
+        if pay_ratio != -999999:
+            div_coverage = round((1 / pay_ratio) * 100, 2)
+        else:
+            div_coverage = -999999
 
-    try:
-        if stock.info["debtToEquity"] and not isinstance(stock.info["debtToEquity"], str):
-            debt_to_equity = round(stock.info["debtToEquity"], 2)
-    except KeyError:
-        debt_to_equity = -999999
+        div_growth_rate = round(calculate_dividend_growth_rate(stock), 2)
 
-    try:
-        if stock.info["trailingPE"] and not isinstance(stock.info["trailingPE"], str):
-            pe_ratio = round(stock.info["trailingPE"], 2)
-    except KeyError:
-        pe_ratio = -999999
+        try:
+            if stock.info["trailingEps"] and not isinstance(stock.info["trailingEps"], str):
+                eps = round(stock.info["trailingEps"], 2)
+        except KeyError:
+            eps = -999999
 
-    try:
-        if stock.info["returnOnEquity"] and not isinstance(stock.info["returnOnEquity"], str):
-            roe = round(stock.info["returnOnEquity"], 2)
-    except KeyError:
-        roe = -999999
+        try:
+            if stock.info["debtToEquity"] and not isinstance(stock.info["debtToEquity"], str):
+                debt_to_equity = round(stock.info["debtToEquity"], 2)
+        except KeyError:
+            debt_to_equity = -999999
 
-    # additional metrics have been added
-    try:
-        if stock.info["bookValue"] and not isinstance(stock.info["bookValue"], str):
-            book_value = round(stock.info["bookValue"], 2)
-    except KeyError:
-        book_value = -999999
+        try:
+            if stock.info["trailingPE"] and not isinstance(stock.info["trailingPE"], str):
+                pe_ratio = round(stock.info["trailingPE"], 2)
+        except KeyError:
+            pe_ratio = -999999
 
-    delta_price_book = round(float(price) - float(book_value), 2)
+        try:
+            if stock.info["returnOnEquity"] and not isinstance(stock.info["returnOnEquity"], str):
+                roe = round(stock.info["returnOnEquity"], 2)
+        except KeyError:
+            roe = -999999
 
-    try:
-        if stock.info["pegRatio"] and not isinstance(stock.info["pegRatio"], str):
-            peg = round(stock.info["pegRatio"], 2)
-    except KeyError:
-        peg = -999999
+        # additional metrics have been added
+        try:
+            if stock.info["bookValue"] and not isinstance(stock.info["bookValue"], str):
+                book_value = round(stock.info["bookValue"], 2)
+        except KeyError:
+            book_value = -999999
 
-    dic = {
-        "ticker": t,
-        "name": name,
-        "beta": beta,
-        "price": price,
-        "delta_price_book": delta_price_book,
-        "dividend_yield": div_yield,
-        "yearly_dividend_yield": yearly_div_yield,
-        "payout_ratio": pay_ratio,
-        "dividend_growth_rate": div_growth_rate,
-        "eps": eps,
-        "pe_ratio": pe_ratio,
-        "peg_ratio": peg,
-        "debt_to_equity": debt_to_equity,
-        "roe": roe,
-        "div_coverage_ratio": div_coverage
-    }
+        delta_price_book = round(float(price) - float(book_value), 2)
 
-    # exclude stocks with no dividend yield and those with a yield lower than 8%
-    if dic["dividend_yield"] != -999999 and dic["dividend_yield"] > 0 and dic["yearly_dividend_yield"] >= 8:
-        stock_data.append(dic)
-    else:
-        stock_with_no_yield.append(dic)
+        try:
+            if stock.info["pegRatio"] and not isinstance(stock.info["pegRatio"], str):
+                peg = round(stock.info["pegRatio"], 2)
+        except KeyError:
+            peg = -999999
+
+        dic = {
+            "ticker": t,
+            "name": name,
+            "industry": industry,
+            "sector": sector,
+            "beta": beta,
+            "price": price,
+            "delta_price_book": delta_price_book,
+            "dividend_yield": div_yield,
+            "yearly_dividend_yield": yearly_div_yield,
+            "payout_ratio": pay_ratio,
+            "dividend_growth_rate": div_growth_rate,
+            "eps": eps,
+            "pe_ratio": pe_ratio,
+            "peg_ratio": peg,
+            "debt_to_equity": debt_to_equity,
+            "roe": roe,
+            "div_coverage_ratio": div_coverage
+        }
+
+        # exclude stocks with no dividend yield and those with a yearly yield lower than 10%
+        if dic["dividend_yield"] != -999999 and dic["yearly_dividend_yield"] >= 10 and dic["div_coverage_ratio"] >= 1.5:
+            stock_data.append(dic)
+        else:
+            stock_with_no_yield.append(dic)
 
 stock_data = [data for data in stock_data if data is not None]  # Filter out stocks with missing data
 
@@ -150,7 +169,15 @@ stock_df[['positives', 'negatives']] = stock_df.apply(lambda row: calculate_metr
 st.title("Dividend Stock Screener")
 
 st.write("Select sorting criteria:")
-criteria = st.selectbox("Sort by", ["Dividend Yield", "Payout Ratio", "Dividend Growth Rate", "EPS", "P/E Ratio", "Debt-to-Equity Ratio", "ROE", "positives"])
+criteria = st.selectbox("Sort by", ["Dividend Yield",
+                                    "Yearly Dividend Yield",
+                                    "Payout Ratio",
+                                    "Dividend Growth Rate",
+                                    "EPS",
+                                    "P/E Ratio",
+                                    "Debt-to-Equity Ratio",
+                                    "ROE",
+                                    "positives"])
 
 sorted_df = stock_df.sort_values(by=criteria.replace(" ", "_").lower(), ascending=False)
 sorted_df = sorted_df.dropna()  # Remove rows with any missing data
